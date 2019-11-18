b32cea0fde1b52ca5211659a37d2209ee8df3b77
Jae Choi <jchoi625@gmail.com>
2019-11-17 22:08:53 -0500

    created js folder and idodeclare.js

---
+++ WebFundamentals_2019/JavaScript/IDoDeclare.js
@@ -0,0 +1,35 @@
+/* Declare variables of the following types:
+4 Numbers
+3 Strings
+2 Booleans
+1 Undefined Variable
+Give the variables descriptive names (ex: string1 and string2)
+Print each one to the console with a description (ex: console.log("The First String:", string1);) */
+
+// Numbers
+var num1 = 920;
+var num2 = 420;
+var num3 = 625;
+var num4 = 503;
+console.log("The first number:", num1);
+console.log("The second number:", num2);
+console.log("The third number:", num3);
+console.log("The fourth number:", num4);
+
+// Strings
+var str1 = "Monday Blues";
+var str2 = "Thanksgiving";
+var str3 = "Protein Shake";
+console.log("The first string:", str1);
+console.log("The second string:", str2);
+console.log("The third string:", str3);
+
+//Booleans
+var bool1 = true;
+var bool2 = false;
+console.log("The first boolean:", bool1);
+console.log("The second boolean:", bool2);
+
+// Undefined
+var undef = undefined;
+console.log("The undefined value:", undef);
